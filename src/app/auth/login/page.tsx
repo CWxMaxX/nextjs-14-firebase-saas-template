6516ad6b671efaa92352f11c9d1763ead084fcd1
Chamith Wijesooriya <chamithwijesooriya@gmail.com>
2024-06-25 20:18:41 +0530

    hosted into firebase

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='w-full min-h-screen flex justify-center items-center'>
+        <div className='w-[450px] min-h-[300px] rounded-xl border border-gray-400 text-gray-600 text-center p-2' >
+            Login Page
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ src/app/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAl0qxMBgz1eTV5yVLnl10X1g1S1t6pXg4",
+  authDomain: "cwx-basic-template.firebaseapp.com",
+  projectId: "cwx-basic-template",
+  storageBucket: "cwx-basic-template.appspot.com",
+  messagingSenderId: "617905175772",
+  appId: "1:617905175772:web:7d27e22ef379732d66c819"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
