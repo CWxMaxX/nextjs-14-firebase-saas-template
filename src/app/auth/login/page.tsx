7f349bbaa10985a89cecb22a77237491efbb5867
Chamith Wijesooriya <chamithwijesooriya@gmail.com>
2024-09-10 22:29:13 +0530

    code formatted

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -6,75 +6,75 @@ import Image from 'next/image';
 import useFirebaseAuth from '../../../hooks/useFirebaseAuth';
 
 const styles = {
-    width: '100%',
-    marginBottom: 16
+  width: '100%',
+  marginBottom: 16
 };
 
 const LoginPage: React.FC = () => {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const { user, error, loading, login, loginWithGoogle } = useFirebaseAuth();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { user, error, loading, login, loginWithGoogle } = useFirebaseAuth();
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-            await login(email, password);   
-        } catch (error) {
-            console.log(error)
-           
-        }
-    };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+    } catch (error) {
+      console.log(error)
 
-    const handleLoginWithGoogle = async (e: React.FormEvent) => { 
-        e.preventDefault();
-        try {
-            await loginWithGoogle()
-        } catch (error) {
-            console.log(error)
-        }
+    }
+  };
 
-     }
+  const handleLoginWithGoogle = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await loginWithGoogle()
+    } catch (error) {
+      console.log(error)
+    }
 
-     useEffect(() => {
-        console.log("User : ", user)
-     }, [user])
-     
+  }
 
-    return (
-        <div className='w-full min-h-screen flex justify-center items-center bg-slate-100'>
-            <div className='sm:max-w-[450px] w-full h-screen sm:h-fit min-h-[300px] rounded-xl shadow-xl bg-white text-gray-700 text-center pt-32 sm:p-10 p-10 items-center flex flex-col' >
-                <div className='w-full text-right'>
-                    <Link href='/auth/signup' className='hover:text-black' >Create new Account</Link>
-                </div>
-                <div className='w-full' >
-                    <Image width="50" height="50" src="https://img.icons8.com/374151/ios-filled/50/canva-app.png" alt="canva-app" />
-                </div>
-                <div className='w-full text-left mt-2 font-extrabold text-2xl' >Welcome Back !</div>
-                <div className='w-full text-left mb-8 tracking-widest' >Enter your details to get started.</div>
-                <InputGroup size='lg' inside style={styles}>
-                    <InputGroup.Addon>
-                        <Image width="20" height="20" src="https://img.icons8.com/717883/parakeet-line/48/new-post.png" alt="new-post" />
-                    </InputGroup.Addon>
-                    <Input placeholder='Email' type="email"
-                        value={email}
-                        onChange={(value: string) => setEmail(value)}
-                        required />
-                </InputGroup>
-                <InputGroup size='lg' inside style={styles}>
-                    <InputGroup.Addon>
-                        <Image width="20" height="20" src="https://img.icons8.com/717883/ios/48/key.png" alt="key" />
-                    </InputGroup.Addon>
-                    <Input placeholder='Password' type="password"
-                        value={password}
-                        onChange={(value: string) => setPassword(value)}
-                        required />
-                </InputGroup>
-                <Button onClick={handleSubmit} appearance='primary' style={styles} size='lg' >Login</Button>
-                <div className='w-full border-t pt-2 border-slate-200 mb-4 text-sm text-slate-400'>or continue with</div>
-                <Button onClick={handleLoginWithGoogle} appearance='ghost' size='lg' style={styles} startIcon={<Image width="24" height="24" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo" />} >Google</Button>
-            </div>
+  useEffect(() => {
+    console.log("User : ", user)
+  }, [user])
+
+
+  return (
+    <div className='w-full min-h-screen flex justify-center items-center bg-slate-100'>
+      <div className='sm:max-w-[450px] w-full h-screen sm:h-fit min-h-[300px] rounded-xl shadow-xl bg-white text-gray-700 text-center pt-32 sm:p-10 p-10 items-center flex flex-col' >
+        <div className='w-full text-right'>
+          <Link href='/auth/signup' className='hover:text-black' >Create new Account</Link>
         </div>
-    )
+        <div className='w-full' >
+          <Image width="50" height="50" src="https://img.icons8.com/374151/ios-filled/50/canva-app.png" alt="canva-app" />
+        </div>
+        <div className='w-full text-left mt-2 font-extrabold text-2xl' >Welcome Back !</div>
+        <div className='w-full text-left mb-8 tracking-widest' >Enter your details to get started.</div>
+        <InputGroup size='lg' inside style={styles}>
+          <InputGroup.Addon>
+            <Image width="20" height="20" src="https://img.icons8.com/717883/parakeet-line/48/new-post.png" alt="new-post" />
+          </InputGroup.Addon>
+          <Input placeholder='Email' type="email"
+            value={email}
+            onChange={(value: string) => setEmail(value)}
+            required />
+        </InputGroup>
+        <InputGroup size='lg' inside style={styles}>
+          <InputGroup.Addon>
+            <Image width="20" height="20" src="https://img.icons8.com/717883/ios/48/key.png" alt="key" />
+          </InputGroup.Addon>
+          <Input placeholder='Password' type="password"
+            value={password}
+            onChange={(value: string) => setPassword(value)}
+            required />
+        </InputGroup>
+        <Button onClick={handleSubmit} appearance='primary' style={styles} size='lg' >Login</Button>
+        <div className='w-full border-t pt-2 border-slate-200 mb-4 text-sm text-slate-400'>or continue with</div>
+        <Button onClick={handleLoginWithGoogle} appearance='ghost' size='lg' style={styles} startIcon={<Image width="24" height="24" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo" />} >Google</Button>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,78 +3,78 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 
 // Define User Context Type
 interface UserContextType {
-    isLoggedIn: boolean;
-    userName: string;
-    uid: string;
-    email: string;
-    setUser: React.Dispatch<
-        React.SetStateAction<{
-            isLoggedIn: boolean;
-            userName: string;
-            uid: string;
-            email: string;
-        }>
-    >;
-    logout: () => void
+  isLoggedIn: boolean;
+  userName: string;
+  uid: string;
+  email: string;
+  setUser: React.Dispatch<
+    React.SetStateAction<{
+      isLoggedIn: boolean;
+      userName: string;
+      uid: string;
+      email: string;
+    }>
+  >;
+  logout: () => void
 }
 
 // Create User Context with Default Values
 const UserContext = createContext<UserContextType>({
-    isLoggedIn: false,
-    userName: "",
-    uid: "",
-    email: "",
-    setUser: () => { },
-    logout: () => { }
+  isLoggedIn: false,
+  userName: "",
+  uid: "",
+  email: "",
+  setUser: () => { },
+  logout: () => { }
 });
 
 export const AuthContextProvider = ({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) => {
-    const [user, setUser] = useState(() => {
-        // On first load, check if there is any user in local storage
-        const savedUser = localStorage.getItem("user");
-        return savedUser
-            ? JSON.parse(savedUser)
-            : {
-                isLoggedIn: false,
-                userName: "",
-                uid: "",
-                email: "",
-            };
+  const [user, setUser] = useState(() => {
+    // On first load, check if there is any user in local storage
+    const savedUser = localStorage.getItem("user");
+    return savedUser
+      ? JSON.parse(savedUser)
+      : {
+        isLoggedIn: false,
+        userName: "",
+        uid: "",
+        email: "",
+      };
+  });
+
+  useEffect(() => {
+    // Whenever the user state changes, update localStorage
+    if (user.isLoggedIn) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+
+  // Define the logout function
+  const logout = () => {
+    setUser({
+      isLoggedIn: false,
+      userName: "",
+      uid: "",
+      email: "",
     });
+    localStorage.removeItem("user"); // Clear localStorage on logout
+    window.location.replace('/auth/login')
+  };
 
-    useEffect(() => {
-        // Whenever the user state changes, update localStorage
-        if (user.isLoggedIn) {
-            localStorage.setItem("user", JSON.stringify(user));
-        } else {
-            localStorage.removeItem("user");
-        }
-    }, [user]);
-
-    // Define the logout function
-    const logout = () => {
-        setUser({
-            isLoggedIn: false,
-            userName: "",
-            uid: "",
-            email: "",
-        });
-        localStorage.removeItem("user"); // Clear localStorage on logout
-        window.location.replace('/auth/login')
-    };
-
-    return (
-        <UserContext.Provider value={{ ...user, setUser, logout }}>
-            {children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={{ ...user, setUser, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 // Custom Hook to Access User Context
 export const useUserAuth = () => {
-    return useContext(UserContext);
+  return useContext(UserContext);
 };
